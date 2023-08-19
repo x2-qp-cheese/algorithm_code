2a755e12178db9fb17963f738e2a456aebc6d976
x2-qp-cheese <pgw053024@naver.com>
2023-08-19 21:00:53 +0900

    algorithm 8/19

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,3 @@
-#include <iostream>
-using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    int x, y;
-    int xmin=100000;
-    int ymin=100000;
-    int xmax=1;
-    int ymax=1;
-    for (int i = 0; i < n; i++){
-        cin >> x >> y;
-        if (x > xmax)
-            xmax = x;
-        if (y > ymax)
-            ymax = y;
-        if (x < xmin)
-            xmin = x;
-        if (y < ymin)
-            ymin = y;
-    }
-
-    cout << (xmax-xmin)*(ymax-ymin);
-}
+                check = 1;
+                break;
